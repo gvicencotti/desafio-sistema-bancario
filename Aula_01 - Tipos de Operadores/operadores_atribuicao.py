fabf651bdc8b02a2baf7f8e4648f6df60cae49f4
Gustavo Vicençotti <75177988+gvicencotti@users.noreply.github.com>
2024-07-01 09:32:49 -0300

    Delete Aula_01 - Tipos de Operadores directory

--- Aula_01 - Tipos de Operadores/operadores_atribuicao.py
+++
@@ -1,3 +0,0 @@
-saldo = 500 # Atribuição simples
-saldo += 200 # Atribuição de Adição de 200
-saldo -= 100 # Atribuiição de Subtração de 100
--- Aula_01 - Tipos de Operadores/operadores_comparacao.py
+++
@@ -1,9 +0,0 @@
-saldo = 450
-saque = 250
-
-print(saldo == saque) # Igual
-print(saldo != saque) # Diferente
-print(saldo > saque) # Maior que
-print(saldo < saque) # Menor que
-print(saldo >= saque) # Maior ou igual
-print(saldo <= saque) # Menor ou igual
